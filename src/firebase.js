766c918739545c5a279bb2ec3e2b2d735a176347
Mamadou <yagam900@gmail.com>
2026-05-10 11:19:19 +0000

    🔐 Firebase env variables pour Vercel

--- src/firebase.js
+++ src/firebase.js
@@ -4,18 +4,18 @@ import { getFirestore } from 'firebase/firestore'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2GUIlV4Wzg8LLMdHV4fz3P8ET0BM4uIw",
-  authDomain: "gameline-2fbce.firebaseapp.com",
-  projectId: "gameline-2fbce",
-  storageBucket: "gameline-2fbce.firebasestorage.app",
-  messagingSenderId: "150646809588",
-  appId: "1:150646809588:web:150646809588:web:468fbe8d32845355c48fa1",
-  databaseURL: "https://gameline-2fbce-default-rtdb.europe-west1.firebasedatabase.app"
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL:       import.meta.env.VITE_FIREBASE_DB_URL,
 }
 
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const db   = getFirestore(app)
 export const rtdb = getDatabase(app)
 export default app
